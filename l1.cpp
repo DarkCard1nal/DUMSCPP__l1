69716f2a2dd0176adcf315a7808a905d436cb2cf
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-11-20 14:42:16 +0200

    The main function of the l1.cpp file was written, CMake files were changed to support C++23 standard.

--- l1.cpp
+++ l1.cpp
@@ -1,18 +1,51 @@
-﻿#include "l1.h"
+﻿#include <iostream>
 #include <vector>
+#include <map>
+#include <print>
 #include <string>
-using namespace std;
 
 int main(int argc, char *argv[])
 {
 	std::vector<std::string> arguments(argv, argv + argc);
 	arguments.erase(arguments.begin());
 
-	std::cout << arguments.size() << '\n';
+	std::map<std::string, std::vector<std::string *>> countArgs;
+	std::string str, fullStr, word;
+	size_t pos, tmp;
 
-	for (const auto &arg : arguments)
+	for (auto it = arguments.begin(); it != arguments.end(); ++it)
 	{
-		std::cout << arg << '\n';
+		str = *it;
+		fullStr = str;
+		tmp = str.rfind('.');
+		if (tmp == std::string::npos)
+		{
+			countArgs[""].push_back(&(*it));
+		}
+		else
+		{
+			do
+			{
+				pos = tmp;
+				word = fullStr.substr(pos);
+				countArgs[word].push_back(&(*it));
+				str = str.substr(0, pos);
+				tmp = str.rfind('.');
+			} while (tmp != std::string::npos && pos - tmp > 1);
+		}
+	}
+	
+	std::println("the number of files: {}", arguments.size());
+	std::println("types of extensions: {}", countArgs.size());
+	std::println("List of extensions and files:");
+
+	for (const auto &pair : countArgs)
+	{
+		std::println("'{}' : {}", pair.first, pair.second.size());
+		for (const auto &ptr : pair.second)
+		{
+			std::println("\t{}", *ptr);
+		}
 	}
 
 	return 0;
